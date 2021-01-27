9c1ccdd741049c8a354b444f0c59a10967aa7287
khaliqyar18 <khaliqyar@itt-net.com>
2021-01-26 20:53:54 -0500

    Sun added but with errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.Sun =1;
+        FactorX.Moons = 2;
+        FactorX.planets = 3;
+
+
+        SUN Freon = new SUN();
+        Freon.Heat = "500kj";
+        Freon.Radius = "7000km";
+        Freon.Tempreture = 250;
+
     }
 }
--- src/com/company/SolarSystem.java
+++ src/com/company/SolarSystem.java
@@ -2,9 +2,15 @@ package com.company;
 
 public class SolarSystem {
 
-    // We completed the specification. next we go commit our code. 
-    String Sun;
-    String Moon;
-    String Planets;
+    SolarSystem FactorX = new SolarSystem();
+    FactorX.Sun =1;
+    FactorX.Moons = 2;
+    FactorX.planets = 3;
+
+
+    SUN Freon = new SUN();
+    Freon.Heat = "500kj";
+    Freon.Radius = "7000km";
+    Freon.Tempreture = 250;
 
 }
