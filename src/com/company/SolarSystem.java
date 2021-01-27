39d7b395b05a58e54ecc28d4bc519c06eeb723a1
khaliqyar18 <khaliqyar@itt-net.com>
2021-01-26 20:41:37 -0500

    Solar System Specifications Ready

---
+++ src/com/company/SolarSystem.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class SolarSystem {
+
+    // We completed the specification. next we go commit our code. 
+    String Sun;
+    String Moon;
+    String Planets;
+
+}
